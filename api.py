b698ceacf0af1fa72bd8bf2f296e3a49f0d2585b
Ralph Lauren Alquizar <ralquizar@umindanao.edu.ph>
2023-12-13 20:48:42 +0800

    first commit

---
+++ api.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  print ('Hello World')
+
+
+if __name__ == '__main__':
+  app.run()
+
+  
